f43f8c7e5de28920403a8dc82d68b95a21c9e0de
RasheedC <borgy_casalmer@yahoo.com>
2024-02-08 11:33:38 -0700

    render values examples

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 "use strict";
-
+``;
 let firstString = "this is our first string. yay!";
 let firstNumber = 42
 
 //connect to html elements
 const strElement = document.querySelector("#first-string");
 const numElement = document.querySelector("#first-number");
-
+const userNameEl = document.querySelector("#user-name");
+const userIdEl = document.querySelector("#user-id");
 /*
 ways to add information to html:
 1.textContent == only text, not style aware
@@ -16,4 +17,23 @@ ways to add information to html:
 */
 
 strElement.textContent = firstString;
-numElement.textContent = firstNumber;
+numElement.textContent = "the meaning of life";
+
+// user examples
+
+let userName;
+
+if (!userName) {
+  userNameEl.textContent = "User is not logged in";
+} else {
+  userNameEl.textContent = userName;
+}
+
+let userID;
+
+if (!userID) {
+  const message = "enter user id";
+  userIdEl.textContent = message;
+} else {
+  userIdEl.textContent = "userId";
+}
